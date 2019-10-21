0413ddc1a6ef4f0fb3ea5d43215b2c84abdfab84
Emil Musagitov <tiger.1911@mail.ru>
2019-10-21 16:01:16 +0500

    ..

---
+++ WebUI/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IBookRepository repository;
+
+        public AdminController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Books);
+        }
+
+        public ViewResult Edit(int bookId)
+        {
+            Book book = repository.Books.FirstOrDefault(b => b.BookId == bookId);
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveBook(book);
+                TempData["message"] = string.Format("Изменение информации о товаре \"{0}\" сохранены", book.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(book);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int bookId)
+        {
+            Book deletedBook = repository.DeleteBook(bookId);
+            if (deletedBook != null)
+            {
+                TempData["message"] = string.Format("Товар \"{0}\" был удален",
+                    deletedBook.Name);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Book());
+        }
+
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Abstract;
+using Domain.Concrete;
+//using Domain.Concrete.Domain.Concrete;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            //mock.Setup(m => m.Books).Returns(new List<Book>
+            //{
+            //    new Book {Name="Язык программирования C# 5.0", Author="Трофимов",Price = 1179 },
+            //    new Book {Name="Язык программирования C++", Author="Бритни",Price = 1155 },
+            //    new Book {Name="Язык программирования C", Author="Катни",Price = 359 }
+            //});
+            //kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IBookRepository>().To<EFBookRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
